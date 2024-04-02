9d27ec905d09803fc247e43ea863f2f2f34cc34a
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-04-02 14:48:51 +0200

    commit sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import SidebarRight from "./components/SidebarRight";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <SidebarRight />
       <Footer />
     </BrowserRouter>
   );
