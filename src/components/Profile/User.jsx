986d17c169b11690ae0356c96535c8e4fbfc7df0
Vorshim92 <vorshim92@gmail.com>
2024-04-04 17:01:56 +0200

    usermodal

--- src/components/Profile/User.jsx
+++ src/components/Profile/User.jsx
@@ -7,7 +7,7 @@ import { fetchUser } from "../../redux/actions/fetchUser";
 import UserImage from "./UserImageModal";
 import ExpUserModal from "./ExpUserModal";
 import { fetchUserExp } from "../../redux/actions/fetchUser";
-
+import UserInfoModal from "./UserInfoModal";
 import Experience from "./Experience";
 
 const User = () => {
@@ -15,6 +15,7 @@ const User = () => {
   const userData = useSelector((state) => state.user.userData);
   const [showImg, setShowImg] = useState(false);
   const [showExp, setShowExp] = useState(false);
+  const [showUsr, setShowUsr] = useState(false);
   const userExp = useSelector((state) => state.exp.expData);
 
   const toggleModalImage = () => {
@@ -22,6 +23,9 @@ const User = () => {
   };
   const toggleExpModal = () => {
     setShowExp(!showExp);
+  };
+  const toggleUsrModal = () => {
+    setShowUsr(!showUsr);
   };
 
   useEffect(() => {
@@ -38,37 +42,48 @@ const User = () => {
     <>
       <Card id="main-card" className="mt-3">
         <Card.Img variant="top" src="userbackground.svg" alt="User Background" className="position-relative" />
-        <div
-          className="position-absolute"
-          style={{
-            right: "20px",
-            top: "20px",
-          }}
-        >
-          <Button onClick={toggleModalImage} id="btnModify">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="camera-small" role="none" data-supported-dps="16x16" fill="#0A66C2" fillOpacity="0.9" width="16" height="16">
-              <path d="M10 9a2 2 0 11-2-2 2 2 0 012 2zm5-2.5V14H1V6.5A2.5 2.5 0 013.5 4h.75L5 2h6l.75 2h.75A2.5 2.5 0 0115 6.5zM11 9a3 3 0 10-3 3 3 3 0 003-3z" fillOpacity="0.9"></path>
-            </svg>
-          </Button>
-          <UserImage showImg={showImg} toggleModalImage={toggleModalImage} />
-        </div>
-        <Card.Body style={{ position: "relative" }}>
-          <img
-            src={userData.image}
-            alt=""
-            style={{
-              width: "150px",
-              height: "150px",
-              position: "absolute",
-              top: "0%",
-              left: "3%",
-              transform: "translateY( -70%)",
-              borderRadius: "50%",
-              border: "5px solid white",
-              objectFit: "cover",
-            }}
-          />
-          <Row className="p-3">
+
+        <UserImage showImg={showImg} toggleModalImage={toggleModalImage} />
+
+        <Card.Body style={{}}>
+          <div className="position-relative">
+            <img
+              src={userData.image}
+              alt=""
+              style={{
+                width: "150px",
+                height: "150px",
+                position: "absolute",
+                top: "0%",
+                left: "3%",
+                transform: "translateY( -70%)",
+                borderRadius: "50%",
+                border: "5px solid white",
+                objectFit: "cover",
+              }}
+            />
+            <div
+              className="position-absolute"
+              style={{
+                left: "85px",
+                top: "-45px",
+                zIndex: "2",
+              }}
+            >
+              <Button onClick={toggleModalImage} id="btnModify">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="camera-small" role="none" data-supported-dps="16x16" fill="#0A66C2" fillOpacity="0.9" width="16" height="16">
+                  <path d="M10 9a2 2 0 11-2-2 2 2 0 012 2zm5-2.5V14H1V6.5A2.5 2.5 0 013.5 4h.75L5 2h6l.75 2h.75A2.5 2.5 0 0115 6.5zM11 9a3 3 0 10-3 3 3 3 0 003-3z" fillOpacity="0.9"></path>
+                </svg>
+              </Button>
+            </div>
+          </div>
+          <Row className="p-3 position-relative">
+            <button onClick={toggleUsrModal} id="btnModal2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+              </svg>
+            </button>
+            <UserInfoModal showUsr={showUsr} toggleUsrModal={toggleUsrModal}></UserInfoModal>
             <Col xs={12} className="mt-5">
               <Row>
                 <Col xs={12} lg={3}>
@@ -161,10 +176,7 @@ const User = () => {
       {/* ---> inizio card esperienze<--- */}
       <Card className="rounded-3 my-3 mt-2">
         <Card.Body className="p-3">
-          <Card.Title
-            className="d-flex justify-content-between
-          "
-          >
+          <Card.Title className="d-flex justify-content-between">
             <p>Esperienze</p>
             <div>
               <button onClick={toggleExpModal} id="btnModal1">
@@ -181,36 +193,6 @@ const User = () => {
       </Card>
 
       {/* --->fine card esperienze<--- */}
-
-      {/* --->Inizio card Licenze e certificazioni<--- */}
-      <Card className="rounded-3 my-3 mt-2">
-        <Card.Body className="p-3">
-          <Card.Title
-            className="d-flex justify-content-between
-          "
-          >
-            <p>Licenze e certificazioni</p>
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-              </svg>
-            </div>
-          </Card.Title>
-
-          <div className="d-flex">
-            <img className=" mx-2" src="https://images.pexels.com/photos/374918/pexels-photo-374918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="numbers" style={{ width: "45px", height: "45px" }} />
-
-            <div className="d-flex flex-column">
-              <p className="sidebarTitle m-0">Licenza di uccidere</p>
-              <p className="m-0">Autodidatta</p>
-              <p className="dataLavoro"> gen 1523 - feb 1952 - Russia</p>
-            </div>
-          </div>
-        </Card.Body>
-      </Card>
     </>
   );
 };
