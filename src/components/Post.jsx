4f65c508f23f2fdc8c75e7e5b6cadf25324f011b
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-04-05 12:33:14 +0200

    comment card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,10 +8,12 @@ import { BsHandThumbsUp } from "react-icons/bs";
 import { BsChatText } from "react-icons/bs";
 import { PiArrowsCounterClockwise } from "react-icons/pi";
 import { IoIosSend } from "react-icons/io";
+import Comment from "./Comment";
+import Collapse from "react-bootstrap/Collapse";
 
 const Post = ({ post }) => {
   const userData = useSelector((state) => state.user.userData);
-
+  const [open, setOpen] = useState(false);
   const [showPostMod, setShowPostMod] = useState(false);
   const toggleModalPost = () => {
     setShowPostMod(!showPostMod);
@@ -61,7 +63,14 @@ const Post = ({ post }) => {
                 <Card.Text className="m-0">{post.text}</Card.Text>
                 {userData._id === post.user._id && (
                   <button id="btnModal2" onClick={toggleModalPost}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-pencil"
+                      viewBox="0 0 16 16"
+                    >
                       <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
                     </svg>
                   </button>
@@ -79,7 +88,12 @@ const Post = ({ post }) => {
                 Consiglia
               </Button>
 
-              <Button className="homeBtnPost">
+              <Button
+                className="homeBtnPost "
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+              >
                 <BsChatText className="my-1 mx-2 fs-4" />
                 Commenta
               </Button>
@@ -95,6 +109,12 @@ const Post = ({ post }) => {
               </Button>
             </Col>
           </Card.Body>
+
+          <Collapse in={open}>
+            <div id="example-collapse-text">
+              <Comment />
+            </div>
+          </Collapse>
         </Card.Body>
       </Card>
       <PostHomeModal showModal={showPostMod} handleModal={toggleModalPost} post={post}></PostHomeModal>
