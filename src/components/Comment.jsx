0eb1f97a0b20cda182734aa4d094fec74a58c566
Vorshim92 <vorshim92@gmail.com>
2024-04-05 17:01:37 +0200

    delete comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,23 +1,22 @@
 import Card from "react-bootstrap/Card";
 import { FaTrash } from "react-icons/fa";
-
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { deleteComment, fetchComments } from "../redux/actions/fetchUser";
 const Comment = ({ comment }) => {
   const profiles = useSelector((state) => state.users.usersData);
   const commentAuthor = profiles.filter((profile) => comment.author === profile.username);
+  const dispatch = useDispatch();
+
+  const handleDelete = async (e) => {
+    await dispatch(deleteComment(comment._id));
+    dispatch(fetchComments()).then(() => {});
+  };
 
   return (
     <>
       {commentAuthor && commentAuthor.length > 0 && (
         <div className="d-flex m-4">
-          <img
-            className="rounded-circle m-3 mt-0"
-            style={{ width: "30px", height: "30px" }}
-            src={commentAuthor[0].image}
-            alt="avatar"
-          />
+          <img className="rounded-circle m-3 mt-0" style={{ width: "30px", height: "30px" }} src={commentAuthor[0].image} alt="avatar" />
           <Card className="w-75 bg-body-tertiary border-0 rounded-3">
             <Card.Body>
               <div className="d-flex justify-content-between align-items-start">
@@ -38,6 +37,7 @@ const Comment = ({ comment }) => {
               <p>
                 {comment.comment}{" "}
                 <FaTrash
+                  onClick={handleDelete}
                   style={{
                     border: "none",
                     cursor: "pointer",
